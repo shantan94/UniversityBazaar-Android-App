cfc9191950d1e74b51b3b399dc0fbf299420e080
harsha <harshavarma93@gmail.com>
2018-02-28 00:21:12 -0600

    added lend home page, changed marketplace homepage

--- app/src/main/java/uta/ubs/LendActivity.java
+++ app/src/main/java/uta/ubs/LendActivity.java
@@ -2,15 +2,33 @@ package uta.ubs;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /**
  * Created by shantan on 2/25/2018.
  */
 
 public class LendActivity extends AppCompatActivity {
+
+    ListView lv;
+    ItemService is;
+    ArrayList<Item> list;
+    ItemAdapter ia;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_lend_page);
+        setContentView(R.layout.lend_page);
+        lv = (ListView) findViewById(R.id.textView2_lend);
+        list = new ArrayList<>();
+        is = new ItemService();
+        list = is.getItems("Lend");
+        Log.d("here", list.toString());
+        ia = new ItemAdapter(this, list);
+        lv.setAdapter(ia);
     }
 }
+
